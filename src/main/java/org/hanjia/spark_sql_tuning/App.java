ce09d5c79aa2cca20b006450ec149b81d7269fd3
Han Jia <han_jia@apple.com>
2019-08-10 14:48:16 -0700

    first commit

---
+++ src/main/java/org/hanjia/spark_sql_tuning/App.java
@@ -0,0 +1,13 @@
+package org.hanjia.spark_sql_tuning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
